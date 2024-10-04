7ac391302d7e47a5a542680bd3ef8ad2b3ef8bae
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-04 21:38:00 +0200

    Learning: string manipulation.
    Used string interpolations, verbatim identifiers and raw string literals.

---
+++ StringVariablesApp/StringVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath1 = string.Empty;
+string filePath2 = string.Empty;
+
+firstName = "Robin";
+lastName = "Hawiz";
+filePath1 = "C:\\Temp\\files";
+//or
+filePath2 = @"C:\Temp\files";
+//or
+string testString = $@"The file for {firstName} is at C:\temp\files";
+
+//String interpolation.
+Console.WriteLine($"Hi {firstName} {lastName}");
+Console.WriteLine(filePath1 + " is the same as " + filePath2);
+Console.WriteLine(testString);
