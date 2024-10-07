709d6515fa844cda3b59e54fa086aff08f58ad3b
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 10:45:49 +0200

    learing: DateTime, but only surface level stuff

---
+++ DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+DateTime today = DateTime.Now;
+DateTime todayUTC = DateTime.UtcNow;
+
+// DateTime birthday = DateTime.Parse("6/15/1998");
+
+// Dont worry about what culture you're in and how they do formatting. We do our own formatting.
+DateTime birthday = DateTime.ParseExact("6/11/1998", "d/M/yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine(today.ToString("MMM dd, yyyy hh:mm tt zzz"));
