0f6a8030053b7f1cf9d85404832547310c4c5a26
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 09:26:37 +0200

    learning: Int32, UInt32, methods

---
+++ IntVariablesApp/IntVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿// Behind the scenes this is Int32
+int age = 0;
+
+age = 43;
+
+// signed Int32 can be two billion +/-, because the last bit is used to mark the number as positive or negative
+// unsigned UInt32 - 4 billion, because the last bit is not used as a marker anymore, and is instead used as a number
+// bit - 0 or 1
+// byte - 8 bits - 00000000
+// Int32 can hold a 32 bit integer value
+
+Console.WriteLine(age);
+// Is the same as writing...
+Console.WriteLine(age.ToString());
+
+// Don't divide ints - Console.WriteLine(age / 2);
