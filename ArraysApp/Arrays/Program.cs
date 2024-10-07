107a35b2e7c4f609b8acb95c767930f70a4274f4
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 12:05:58 +0200

    learning: arrays, new string[] {} or new string[number] and method Split

---
+++ ArraysApp/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] firstNames = new string[5];
+
+firstNames[0] = "Robin";
+firstNames[1] = "Mary";
+firstNames[2] = "Sofia";
+firstNames[4] = "Jane";
+
+Console.WriteLine(firstNames[1]);
+
+firstNames[1] = "Bob";
+
+Console.WriteLine(firstNames[1]);
+
+// single quote identifies a char
+string data = "Robin,Mary,Sofia,Bob,Tim";
+string[] firstNamesData = data.Split(',');
+Console.WriteLine(firstNamesData[0]);
+Console.WriteLine(firstNamesData.Length);
+
+string[] lastNames = new string[] { "Robin", "Mary", "Sofia" };
+
+int[] ages = new int[] { 2, 3, 4 };
