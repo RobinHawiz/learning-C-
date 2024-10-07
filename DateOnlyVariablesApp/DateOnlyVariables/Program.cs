1e3555182530e3222375f37398a41946fbf18574
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 11:11:09 +0200

    learing: DateOnly, works with .NET 6 and beyond

---
+++ DateOnlyVariablesApp/DateOnlyVariables/Program.cs
@@ -0,0 +1,8 @@
+﻿DateTime today = DateTime.Now;
+DateOnly birthday = DateOnly.Parse("1998/5/3");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
