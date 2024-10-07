9a34046a658c8c6ac1353eef6fdc94e909696306
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 12:29:55 +0200

    learning: Dictionary, uses keys bound to values

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+lookup["animal"] = "not a human that lives on land";
+lookup["fish"] = "not a human that lives underwater";
+lookup["human"] = "a human";
+
+Console.WriteLine($"The definition of fish is {lookup["fish"]}");
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[95] = "Robin Bokström";
+employees[28] = "John Doe";
+
+Console.WriteLine($"The employee with ID number 28 is {employees[28]}");
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+
+dayOfWeek["Monday"] = 1;
+dayOfWeek["Tuesday"] = 2;
+dayOfWeek["Wednesday"] = 3;
+
+Console.WriteLine($"Monday is day number {dayOfWeek["Monday"]}");
