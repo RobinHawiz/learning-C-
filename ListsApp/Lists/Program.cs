2d083077e76cd06b1b0a1c28a904815540ffc127
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-07 12:16:29 +0200

    learning: Lists, the modern way of storing data

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> firstNames = new List<string>();
+
+firstNames.Add("Robin");
+firstNames.Add("Ronald");
+firstNames.Add("McDonald");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+Console.WriteLine(ages[ages.Count - 1]);
+
+string data = "Robin,Ronald,McDonald";
+List<string> lastNames = data.Split(',').ToList();
+lastNames.Add("Smith");
