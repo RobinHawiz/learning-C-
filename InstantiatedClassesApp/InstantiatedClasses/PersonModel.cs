b9386b98f7a73ae98bc55f795544ed1fa5dbccce
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-08 11:06:36 +0200

    learning: Instantiated classes, used to instantiate objects from a class

---
+++ InstantiatedClassesApp/InstantiatedClasses/PersonModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedClasses
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // public int Age { get; set; }
+
+        // private backing field
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 0 && value < 126)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age needs to be in a valid range.");
+                }
+            }
+        }
+
+        // private backing field
+        private string _ssn;
+
+        public string SSN
+        {
+            get
+            {
+                // Swedish social security number format 123456-7891 -> 123456-****
+                string output = _ssn.Substring(0, _ssn.Length - 5) + "-****";
+                return output;
+            }
+            set
+            {
+                _ssn = value;
+            }
+        }
+    }
+}
---
+++ InstantiatedClassesApp/InstantiatedClasses/Program.cs
@@ -0,0 +1,10 @@
+﻿using InstantiatedClasses;
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "Robin";
+person.LastName = "Horton";
+person.Age = 30;
+person.SSN = "123456-7891";
+
+Console.WriteLine(person.SSN);
