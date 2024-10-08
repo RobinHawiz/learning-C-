3a667457ea34e0402e178cbf398c687ad13b0b70
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-08 19:16:21 +0200

    learning: Interfaces, they're a contract. Use them as types when you need common functionality

---
+++ InterfaceApp/Interface/Program.cs
@@ -0,0 +1,90 @@
+﻿List<IComputerController> controllers = new List<IComputerController>();
+
+GameController gameController = new GameController();
+Keyboard keyboard = new Keyboard();
+BatteryPoweredGameController batteryPoweredGameController = new BatteryPoweredGameController();
+BatteryPoweredKeyboard batteryPoweredKeyboard = new BatteryPoweredKeyboard();
+
+controllers.Add(gameController);
+controllers.Add(keyboard);
+controllers.Add(batteryPoweredGameController);
+controllers.Add(batteryPoweredKeyboard);
+
+foreach (IComputerController controller in controllers)
+{
+    if (controller is GameController gc)
+    {
+
+    }
+    if (controller is IBatteryPowered powered)
+    {
+
+    }
+}
+
+List<IBatteryPowered> powereds = new List<IBatteryPowered>();
+
+powereds.Add(batteryPoweredGameController);
+powereds.Add(batteryPoweredKeyboard);
+
+using (GameController gc = new GameController())
+{
+
+}
+
+
+public interface IComputerController : IDisposable
+{
+    void Connect();
+    void CurrentKeyPressed();
+}
+public class Keyboard : IComputerController
+{
+    public void Connect()
+    {
+
+    }
+    public void CurrentKeyPressed()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        // Do whatever shutdown tasks needed.
+    }
+
+    public string ConnectionType { get; set; }
+}
+
+public interface IBatteryPowered
+{
+    int BatteryLevel { get; set; }
+}
+
+public class BatteryPoweredKeyboard : Keyboard, IBatteryPowered
+{
+    public int BatteryLevel { get; set; }
+}
+
+public class GameController : IComputerController, IDisposable
+{
+    public void Connect()
+    {
+
+    }
+    public void CurrentKeyPressed()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        // Do whatever shutdown tasks needed.
+    }
+}
+
+public class BatteryPoweredGameController : GameController, IBatteryPowered
+{
+    public int BatteryLevel { get; set; }
+}
