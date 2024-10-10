0c514d581b061cec44fd0b989e50dfef80279507
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-10 12:45:53 +0200

    learning: Unit testing using xUnit

---
+++ UnitTestApp/MathLibrary.Tests/MathOperationsTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary.Tests;
+
+public class MathOperationsTests
+{
+    [Theory]
+    [InlineData(5, 5, 10)]
+    [InlineData(5, -4, 1)]
+    [InlineData(-5, 4, -1)]
+    public void AddShouldReturnSumOfTwoDoubles(double x, double y, double expected)
+    {
+        double actual = MathOperations.Add(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(4, 5, -1)]
+    [InlineData(5, 4, 1)]
+    public void SubtractShouldReturnDifferenceOfTwoDoubles(double x, double y, double expected)
+    {
+        double actual = MathOperations.Subtract(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(-5, -5, 25)]
+    [InlineData(5, -5, -25)]
+    [InlineData(-5, 5, -25)]
+    [InlineData(5, 0, 0)]
+    [InlineData(0, 5, 0)]
+    public void MultiplyShouldReturnProductOfTwoDoubles(double x, double y, double expected)
+    {
+        double actual = MathOperations.Multiply(x, y);
+
+        Assert.Equal(expected, actual);
+    }
+    [Theory]
+    // Arrange
+    [InlineData(5, 5, 1)]
+    [InlineData(-5, -5, 1)]
+    [InlineData(-5, 5, -1)]
+    [InlineData(5, -5, -1)]
+    [InlineData(0, 5, 0)]
+    [InlineData(-120, 4, -30)]
+    public void DivideShouldReturnQuotientOfTwoDoubles(int x, int y, int expected)
+    {
+        // Act
+        int actual = MathOperations.Divide(x, y);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void DivideShouldReturnDivideByZeroException()
+    {
+        // Arrange 
+        string expected = "Cannot divide by zero";
+
+        // Act
+        Action act = () => MathOperations.Divide(5, 0);
+
+        // Assert
+        DivideByZeroException ex = Assert.Throws<DivideByZeroException>(() => act());
+        Assert.Equal(expected, ex.Message);
+    }
+}
+
---
+++ UnitTestApp/MathLibrary/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary;
+
+public static class MathOperations
+{
+    public static double Add(double x, double y) { return x + y; }
+    public static double Subtract(double x, double y) { return x - y; }
+    public static double Multiply(double x, double y) { return x * y; }
+    // I use Ints here because I want to learn how to catch exceptions and test them in my xunit tests.
+    public static int Divide(int x, int y)
+    {
+        int output = 0;
+        try
+        {
+            output = x / y;
+        }
+        catch (DivideByZeroException ex)
+        {
+            throw new DivideByZeroException("Cannot divide by zero");
+        }
+        return output;
+    }
+}
