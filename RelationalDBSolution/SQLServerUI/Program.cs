6c5802aeeabd81cf631e55c259640d6a259180f4
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-11 15:50:08 +0200

    learning: Setup LocalDB and Dapper

---
+++ RelationalDBSolution/SQLServerUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+static string GetConnectionString(string connectionString = "Default")
+{
+    string output = "";
+
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionString);
+
+    return output;
+}
