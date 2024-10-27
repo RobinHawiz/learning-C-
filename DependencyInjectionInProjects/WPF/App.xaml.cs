292f203827a39e0743628ffb1e384ef8ebd09e74
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-27 14:45:01 +0100

    learning: Dependency injection in a WPF App

---
+++ DependencyInjectionInProjects/WPF/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DIDemoLib;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices(services =>
+            {
+                services
+                    .AddTransient<IMessages, Messages>()
+                    .AddTransient<MainWindow>();
+            });
+
+            var host = builder.Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var form = services.GetRequiredService<MainWindow>();
+                form.Show();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
