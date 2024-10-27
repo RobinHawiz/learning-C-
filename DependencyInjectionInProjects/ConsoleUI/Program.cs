36bfbbfe18ee187db71cf870f5bc7af74d6b1df8
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-27 12:56:00 +0100

    learning: A working version of dependency injection in a Console Application

---
+++ DependencyInjectionInProjects/ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using DIDemoLib;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ConsoleUI;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    services.GetRequiredService<App>().Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error has occured: {ex.Message}");
+}
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services
+            .AddTransient<IMessages, Messages>()
+            .AddTransient<App>();
+    });
