ed7f784432e9aa17cce70d6b80c753363545bd20
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-10-30 17:49:33 +0100

    setup: SQL Server DB

---
+++ DatabaseDevOpsPractice/PersonDB/dbo/StoredProcedures/spLocation_PeopleVisited.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spLocation_PeopleVisited]
+	@LocationId int
+AS
+begin
+
+	set nocount on;
+
+	select *
+	from dbo.[Location] l
+	inner join dbo.LocationVisit v on l.Id = v.LocationId
+	inner join dbo.Person p on p.Id = v.PersonId
+	where l.id = @LocationId;
+
+end
---
+++ DatabaseDevOpsPractice/PersonDB/dbo/Tables/Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [DateOfBirth] DATETIME2 NOT NULL, 
+    [EmailAddress] NVARCHAR(100) NOT NULL
+)
