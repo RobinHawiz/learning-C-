562b650408ad6f0db483974437a106e099431594
Robin Hawiz <34030597+RobinHawiz@users.noreply.github.com>
2024-10-31 18:56:45 +0100

    delete: DatabaseDevOpsPractice directory
    I successfully deployed the database from this directory to a SQL Server on Azure using a CI/CD pipeline.

--- DatabaseDevOpsPractice/PersonDB/dbo/Tables/Location.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Location]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [LocationName] NVARCHAR(50) NOT NULL, 
-    [Location] NVARCHAR(50) NOT NULL, 
-    [Country] NVARCHAR(50) NOT NULL 
-)
--- DatabaseDevOpsPractice/PersonDB/dbo/Tables/Person.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Person]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [DateOfBirth] DATETIME2 NOT NULL, 
-    [EmailAddress] NVARCHAR(100) NOT NULL
-)
