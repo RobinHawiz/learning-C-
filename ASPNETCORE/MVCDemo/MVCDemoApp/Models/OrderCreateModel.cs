9e0ad7fa25baa69c24982f8f2e1ca78f0b065406
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-03 21:22:40 +0100

    MVC create, read and update data using the class library and DinerDB from ASPNETCOREsetup

---
+++ ASPNETCORE/MVCDemo/MVCDemoApp/Models/OrderCreateModel.cs
@@ -0,0 +1,9 @@
+﻿using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVCDemoApp.Models;
+public class OrderCreateModel
+{
+    public OrderModel Order { get; set; } = new OrderModel();
+    public List<SelectListItem> FoodItems { get; set; } = new List<SelectListItem>();
+}
---
+++ ASPNETCORE/MVCDemo/MVCDemoApp/Models/OrderDisplayModel.cs
@@ -0,0 +1,9 @@
+﻿using DataLibrary.Models;
+
+namespace MVCDemoApp.Models;
+public class OrderDisplayModel
+{
+    public OrderModel Order { get; set; }
+    public string ItemPurchased { get; set; }
+
+}
