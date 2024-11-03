5d00223653d242d67e986faeb670cf3796236e5a
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-03 12:07:48 +0100

    learning: Update/delete statement and stored procedures

---
+++ SQLQueryPractice/ExecStoredProcedureQuery.sql
@@ -0,0 +1,6 @@
+
+
+exec dbo.spEmployees_Insert 'Samwise', 'Gamgee', 'samwise@gamgee.com', 300, 400;
+
+select *
+from dbo.Employees;
---
+++ SQLQueryPractice/UpdatesDeletesQueries.sql
@@ -0,0 +1,32 @@
+
+
+
+/*
+update dbo.Employees
+set LastName = 'Peterson'
+where id = 2
+*/
+
+/*
+update dbo.Employees
+set PayRate = 120, BillRate = 170;
+*/
+
+/*
+delete 
+from dbo.Employees
+where id = 5;
+*/
+
+
+/*
+update dbo.Employees
+set LastName = 'Pinche Madre'
+where id = 4
+*/
+
+/*
+delete
+from dbo.WorkDone
+where id = 9
+*/
