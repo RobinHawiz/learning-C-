40980d1431fb4eee11a13503b57ce7ba07c1680c
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-07 20:45:47 +0100

    setup: Initial db tables

---
+++ TravelServiceBackend/TravelServiceDB/db/Tables/Bookings.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Bookings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TransportationId] INT NOT NULL, 
+    [ZoneId] INT NOT NULL,
+    [UserId] INT NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL, 
+    [EndDate] DATETIME2 NOT NULL, 
+    [Wheelchair] BIT NOT NULL DEFAULT 0, 
+    [TotalCost] MONEY NOT NULL, 
+    [AddressFrom] NVARCHAR(100) NOT NULL, 
+    [AddressTo] NVARCHAR(100) NOT NULL, 
+    CONSTRAINT [FK_Bookings_Transportations] FOREIGN KEY (TransportationId) REFERENCES Transportations(Id), 
+    CONSTRAINT [FK_Bookings_Zones] FOREIGN KEY (ZoneId) REFERENCES Zones(Id), 
+    CONSTRAINT [FK_Bookings_Users] FOREIGN KEY (UserId) REFERENCES Users(Id) 
+)
---
+++ TravelServiceBackend/TravelServiceDB/db/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(100) NOT NULL
+)
