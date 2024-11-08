2ed80b70e1827c1045f6f87746ffe9ab28731939
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-08 21:43:20 +0100

    sql: Stored procedures for inserting a new user to db

---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spUsers_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Insert]
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@EmailAddress nvarchar(100),
+	@Password nvarchar(20)
+AS
+	begin
+		set nocount on;
+
+		insert dbo.Users(FirstName, LastName, EmailAddress, [Password])
+		values(@FirstName, @LastName, @EmailAddress, @Password);
+	end
