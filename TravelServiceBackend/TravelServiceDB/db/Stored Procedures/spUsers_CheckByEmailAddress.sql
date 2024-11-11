11a3505e0cec2469a7899bf69c2c3fd63549969c
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-11 20:55:50 +0100

    sql: Additional stored procedures for the Users table

---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spUsers_CheckByEmailAddress.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_GetByEmailAddress]
+	@emailAddress nvarchar(100)
+AS
+begin
+	set nocount on;
+
+	select 1
+	from dbo.Users u
+	where u.EmailAddress = @emailAddress; 
+
+end
---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spUsers_GetByEmailAddressAndPassword.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_GetByEmailAddressAndPassword]
+	@emailAddress nvarchar(100),
+	@password nvarchar(20)
+AS
+begin
+	set nocount on;
+
+	select [u].[Id], [u].[FirstName], [u].[LastName], [u].[EmailAddress]
+	from dbo.Users u
+	where u.EmailAddress = @emailAddress
+	and
+	u.[Password] = @password
+
+end
