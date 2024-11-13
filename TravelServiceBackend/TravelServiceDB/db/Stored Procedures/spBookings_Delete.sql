3a89dc8ef19b27b0eaf82a5f1e2cfa7897493ba3
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-13 19:37:38 +0100

    sql: Additional stored procedures for the Bookings table
    Stored procedures for: deleting a booking by Id, and updating AddressFrom, AddressTo and ZoneId by Id

---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spBookings_Delete.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Delete]
+	@id int
+AS
+begin
+	set nocount on
+
+	delete
+	from dbo.Bookings
+	where Id = @id;
+end
---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spBookings_UpdateAddress.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_UpdateAddress]
+	@Id int,
+	@ZoneId int,
+	@AddressFrom datetime2,
+	@AddressTo datetime2
+
+AS
+begin
+	set nocount on
+
+	update dbo.Bookings
+	set AddressFrom = @AddressFrom
+	  , AddressTo = @AddressTo
+	  , ZoneId = @ZoneId
+	where Id = @Id;
+end
