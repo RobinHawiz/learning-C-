d0932c7dff022dca55bc9c38eb9586bc7d57bc6e
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-14 19:42:24 +0100

    New class library for accessing the SQL server database

---
+++ TravelServiceBackend/DataLibrary/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DataLibrary
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<U, T>(string storedProcedure, U parameters, string connectionStringName);
+        Task<int> SaveData<U>(string storedProcedure, U parameter, string connectionStringName);
+    }
+}
---
+++ TravelServiceBackend/DataLibrary/SqlData.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary;
+public class SqlData : IDataAccess
+{
+    private readonly IConfiguration _config;
+    public SqlData(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<List<T>> LoadData<U, T>(string storedProcedure, U parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            var rows = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+            return rows.ToList();
+        }
+    }
+
+    public async Task<int> SaveData<U>(string storedProcedure, U parameter, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            return await connection.ExecuteAsync(storedProcedure, parameter, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+}
