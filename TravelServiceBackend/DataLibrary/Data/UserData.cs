365d6c5e629d8119b4c391c0a9621036b97cf793
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-18 18:38:01 +0100

    class&method: Created a contructor for class UserData and method GetByEmailAddressAndPassword

--- TravelServiceBackend/DataLibrary/Data/UserData.cs
+++ TravelServiceBackend/DataLibrary/Data/UserData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLibrary.Db;
+using DataLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +9,19 @@ using System.Threading.Tasks;
 namespace DataLibrary.Data;
 public class UserData
 {
+    private readonly IDataAccess _dataAccess;
+    private readonly ConnectionStringData _connectionStringName;
+
+    public UserData(IDataAccess DataAccess, ConnectionStringData ConnectionStringName)
+    {
+        _dataAccess = DataAccess;
+        _connectionStringName = ConnectionStringName;
+    }
+
+    public async Task<UserModel> GetByEmailAddressAndPassword(string EmailAddress, string Password)
+    {
+        return (await _dataAccess.LoadData<dynamic, UserModel>("dbo.spUsers_GetByEmailAddressAndPassword"
+                                                             , new { emailAddress = EmailAddress, password = Password }, _connectionStringName.SqlConnectionName))
+                                                             .FirstOrDefault();
+    }
 }
