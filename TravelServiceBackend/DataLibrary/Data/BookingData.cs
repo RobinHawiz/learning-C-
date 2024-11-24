34c04682bccbaaf53a8dc6d084b0d163a142596a
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 11:32:59 +0100

    class: Implemented public class BookingData + a constructor

---
+++ TravelServiceBackend/DataLibrary/Data/BookingData.cs
@@ -0,0 +1,18 @@
+﻿using DataLibrary.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Data;
+public class BookingData
+{
+    private readonly IDataAccess _dataAccess;
+    private readonly ConnectionStringData _connectionStringName;
+    public BookingData(IDataAccess dataAccess, ConnectionStringData connectionStringName)
+    {
+        _dataAccess = dataAccess;
+        _connectionStringName = connectionStringName;
+    }
+}
