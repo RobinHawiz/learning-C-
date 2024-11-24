f48f75d8245ee27316fd1f0b0531722a3753470c
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 14:08:26 +0100

    sql: Added Stored procedures for table Transportations and Zones
    The stored procedures gets all row values from all records

---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spTransportations_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spTransportations_GetAll]
+AS
+begin
+	set nocount on;
+
+	select t.Id, t.PriceMultiplier, t.Title
+	from dbo.Transportations t;
+end
---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spZones_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spZones_GetAll]
+AS
+begin
+	set nocount on;
+
+	select z.Id, z.Price, z.Title
+	from dbo.Zones z;
+end
