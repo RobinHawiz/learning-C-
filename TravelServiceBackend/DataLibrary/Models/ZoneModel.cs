fd9dc9ff52d0ab1c1e652b762377cf7041c486ee
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 13:37:23 +0100

    Model: Created models TansportationModel and ZoneModel

---
+++ TravelServiceBackend/DataLibrary/Models/ZoneModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models;
+public class ZoneModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
