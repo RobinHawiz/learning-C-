60a241bd2134bb397c6d15d9a4033bb280405eea
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 14:19:39 +0100

    class&method: Created ZoneData with a constructor and methods GetById and GetAll

---
+++ TravelServiceBackend/DataLibrary/Data/ZoneData.cs
@@ -0,0 +1,33 @@
+﻿using DataLibrary.Db;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Data;
+public class ZoneData
+{
+    private readonly IDataAccess _dataAccess;
+    private readonly ConnectionStringData _connectionStringName;
+
+    public ZoneData(IDataAccess dataAccess, ConnectionStringData connectionStringName)
+    {
+        _dataAccess = dataAccess;
+        _connectionStringName = connectionStringName;
+    }
+    public async Task<ZoneModel> GetById(int id)
+    {
+        return (await _dataAccess.LoadData<ZoneModel, dynamic>("dbo.spZones_GetById"
+                                                             , new { Id = id }
+                                                             , _connectionStringName.SqlConnectionName))
+                                                             .First();
+    }
+    public async Task<List<ZoneModel>> GetAll()
+    {
+        return await _dataAccess.LoadData<ZoneModel, dynamic>("dbo.spZones_GetAll"
+                                                            , new { }
+                                                            , _connectionStringName.SqlConnectionName);
+    }
+}
