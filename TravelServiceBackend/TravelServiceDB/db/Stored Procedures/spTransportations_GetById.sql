17816e2c232e2475e73d14983ce040fd696231d3
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 13:33:26 +0100

    sql: Added Stored procedures for table Transportations and Zones
    The stored procedures gets all row values from a record by a specified Id

---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spTransportations_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spTransportations_GetById]
+	@Id int
+AS
+begin
+	set nocount on;
+
+	select t.Id, t.PriceMultiplier, t.Title
+	from dbo.Transportations t
+	where t.Id = @Id;
+end
---
+++ TravelServiceBackend/TravelServiceDB/db/Stored Procedures/spZones_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spZones_GetById]
+	@Id int
+AS
+begin
+	set nocount on;
+
+	select z.Id, z.Price, z.Title
+	from dbo.Zones z
+	where z.Id = @Id;
+end
