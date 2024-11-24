d18b9ef97f2481cdee3594141494e4cc61d5098a
robinhawiz <Robin.hawiz.6670@student.uu.se>
2024-11-24 14:20:11 +0100

    class&method: Created TransportationData with a constructor and methods GetById and GetAll

---
+++ TravelServiceBackend/DataLibrary/Data/TransportationData.cs
@@ -0,0 +1,33 @@
+﻿using DataLibrary.Db;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Data;
+public class TransportationData
+{
+    private readonly IDataAccess _dataAccess;
+    private readonly ConnectionStringData _connectionStringName;
+
+    public TransportationData(IDataAccess dataAccess, ConnectionStringData connectionStringName)
+    {
+        _dataAccess = dataAccess;
+        _connectionStringName = connectionStringName;
+    }
+    public async Task<TransportationModel> GetById(int id)
+    {
+        return (await _dataAccess.LoadData<TransportationModel, dynamic>("dbo.spTransportations_GetById"
+                                                             , new { Id = id }
+                                                             , _connectionStringName.SqlConnectionName))
+                                                             .First();
+    }
+    public async Task<List<TransportationModel>> GetAll()
+    {
+        return await _dataAccess.LoadData<TransportationModel, dynamic>("dbo.spTransportations_GetAll"
+                                                            , new { }
+                                                            , _connectionStringName.SqlConnectionName);
+    }
+}
